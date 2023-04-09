d7c676f8890802e26fc4252011b5630489abe546
Jacob Ibáñez Sánchez <jacobibanez@jacobibanez.com>
2023-04-09 10:37:54 +0200

    Fix wrong implementations of GodotPlugin class

--- app/src/main/java/com/jacobibanez/godot/gpgs/GodotGooglePlayGameServices.kt
+++ app/src/main/java/com/jacobibanez/godot/gpgs/GodotGooglePlayGameServices.kt
@@ -1,11 +1,36 @@
 package com.jacobibanez.godot.gpgs
 
+import android.util.Log
+import com.google.android.gms.games.GamesSignInClient
+import com.google.android.gms.games.PlayGames
+import com.google.android.gms.games.PlayGamesSdk
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
+import org.godotengine.godot.plugin.UsedByGodot
 
-class GodotGooglePlayGameServices(godot: Godot): GodotPlugin(godot) {
+class GodotGooglePlayGameServices(godot: Godot) : GodotPlugin(godot) {
+
+    private val tag: String = GodotGooglePlayGameServices::class.java.simpleName
+
+    private lateinit var gamesSignInClient: GamesSignInClient
 
     override fun getPluginName(): String {
-        TODO("Not yet implemented")
+        return "GodotGooglePlayGameServices"
+    }
+
+    /**
+     * Use this to test that the plugin works
+     */
+    @UsedByGodot
+    fun helloWorld() {
+        Log.i(tag,"Hello World")
+    }
+
+    @UsedByGodot
+    fun initialize() {
+        Log.i(tag, "Initializing Google Play Game Services")
+        PlayGamesSdk.initialize(activity!!)
+
+        gamesSignInClient = PlayGames.getGamesSignInClient(activity!!)
     }
 }
