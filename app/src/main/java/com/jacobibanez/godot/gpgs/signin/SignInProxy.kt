694141b68c345d8b6145598d3bfe3685acdb5a3e
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-12-30 01:28:00 -0300

    refactor: signIn

--- app/src/main/java/com/jacobibanez/godot/gpgs/signin/SignInProxy.kt
+++ app/src/main/java/com/jacobibanez/godot/gpgs/signin/SignInProxy.kt
@@ -4,18 +4,13 @@ import android.util.Log
 import com.google.android.gms.games.GamesSignInClient
 import com.google.android.gms.games.PlayGames
 import com.jacobibanez.godot.gpgs.PLUGIN_NAME
-import com.jacobibanez.godot.gpgs.isUserAuthenticatedFailure
-import com.jacobibanez.godot.gpgs.isUserAuthenticatedSuccess
-import com.jacobibanez.godot.gpgs.requestServerSideAccessFailure
-import com.jacobibanez.godot.gpgs.requestServerSideAccessSuccess
-import com.jacobibanez.godot.gpgs.signInFailure
-import com.jacobibanez.godot.gpgs.signInSuccess
+import com.jacobibanez.godot.gpgs.signals.SignInSignals.userAuthenticated
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin.emitSignal
 
 class SignInProxy(
     private val godot: Godot,
-    private val gamesSignInClient: GamesSignInClient = PlayGames.getGamesSignInClient(godot.activity!!)
+    private val gamesSignInClient: GamesSignInClient = PlayGames.getGamesSignInClient(godot.getActivity()!!)
 ) {
 
     private val tag: String = SignInProxy::class.java.simpleName
@@ -28,35 +23,19 @@ class SignInProxy(
                 emitSignal(
                     godot,
                     PLUGIN_NAME,
-                    isUserAuthenticatedSuccess,
+                    userAuthenticated,
                     task.result.isAuthenticated
                 )
             } else {
                 Log.e(tag, "User not authenticated. Cause: ${task.exception}", task.exception)
-                emitSignal(godot, PLUGIN_NAME, isUserAuthenticatedFailure)
+                emitSignal(
+                    godot,
+                    PLUGIN_NAME,
+                    userAuthenticated,
+                    false
+                )
             }
         }
-    }
-
-    fun requestServerSideAccess(serverClientId: String, forceRefreshToken: Boolean) {
-        Log.d(
-            tag,
-            "Requesting server side access for client id $serverClientId with refresh token $forceRefreshToken"
-        )
-        gamesSignInClient.requestServerSideAccess(serverClientId, forceRefreshToken)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Log.d(tag, "Access granted to server side for user: $serverClientId")
-                    emitSignal(godot, PLUGIN_NAME, requestServerSideAccessSuccess, task.result)
-                } else {
-                    Log.e(
-                        tag,
-                        "Failed to request server side access. Cause: ${task.exception}",
-                        task.exception
-                    )
-                    emitSignal(godot, PLUGIN_NAME, requestServerSideAccessFailure)
-                }
-            }
     }
 
     fun signIn() {
@@ -64,10 +43,15 @@ class SignInProxy(
         gamesSignInClient.signIn().addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 Log.d(tag, "User signed in: ${task.result.isAuthenticated}")
-                emitSignal(godot, PLUGIN_NAME, signInSuccess, task.result.isAuthenticated)
+                emitSignal(
+                    godot,
+                    PLUGIN_NAME,
+                    userAuthenticated,
+                    task.result.isAuthenticated
+                )
             } else {
                 Log.e(tag, "User not signed in. Cause: ${task.exception}", task.exception)
-                emitSignal(godot, PLUGIN_NAME, signInFailure)
+                emitSignal(godot, PLUGIN_NAME, userAuthenticated, false)
             }
         }
     }
